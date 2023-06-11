cb16e243bdd4fe181858f6a5db694b50eab7b7a2
Ahmed Hosny <ahmedHosny.R.M.E@gmail.com>
2023-06-11 11:39:23 +0300

    ready for deploy

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './css/header.css';
+import './css/hero.css';
+import './css/about.css';
+import './css/popular.css';
+import './css/trending.css';
+import './css/subscribe.css';
+import './css/footer.css';
+import './css/style.css';
+
+import './js/script.js';
